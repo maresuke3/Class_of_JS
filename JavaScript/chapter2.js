997f4e06856b58558c6a13307c21dcc3bfe2e24b
daikimare <b1840702@planet.kanazawa-it.ac.jp>
2020-08-12 10:12:59 +0900

    aleartの説明

---
+++ JavaScript/chapter2.js
@@ -0,0 +1,22 @@
+// 変数の宣言(作成)
+var str = "JavaScriptの勉強会を始めます"
+
+// 文字列の長さを調べる
+console.log(str.length);
+
+// 日付を表示してみる
+// 使用するのはDate型変数です！
+var date = new Date();
+
+// 表示する文字列のフォーマットの指定と表示
+console.log(date.toLocaleString());
+
+
+// では前節の復習は終えて実際に動かせるものを作ってみましょう！
+// web上で何かを警告したり確認画面とか出てくる時によく使用されるalertを作ってみましょう
+
+  alert(str);
+
+  
+  // var data = navigator.platform + "\n"
+  // + navigator.userAgent + "\n";
