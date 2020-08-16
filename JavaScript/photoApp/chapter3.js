aec91aa5337f338545ce95d76a9488c24d204866
daikimare <b1840702@planet.kanazawa-it.ac.jp>
2020-08-17 05:09:30 +0900

    コードの説明をコメントアウトに記載

--- JavaScript/photoApp/chapter3.js
+++ JavaScript/photoApp/chapter3.js
@@ -20,12 +20,18 @@ window.addEventListener("DOMContentLoaded", function() {
 });
 
 function jsonFlickrFeed(data) {
+  // HTMLの画像を表示するフィールドの指定。今回の指定フィールドはdivタグ
   let photo_list = document.getElementById("photo_list");
+  // 表示フィールドの初期化(クリア)
   photo_list.innerHTML = "";
 
+  // itemsキーで検索結果を取得保存
   for (let i = 0; i < data.items.length; i++) {
+    // 検索結果の情報変数に格納
     let item = data.items[i];
+    // ブラウザの表示フィールドに表示させるオブジェクトの作成　オブジェクト名はimg
     let img = document.createElement("img");
+    // imgにURLを処理を指定し画像を検索結果を反映させる
     img.src = item.media.m;
     photo_list.appendChild(img);
   }
